928756470ab85edad8e8dcc1d7ed7ae8e7e959b1
Whitneyto <nguyetto@udel.edu>
2021-10-12 17:45:13 -0400

    Add branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler!
-                <p> Julianna Dorsch
+                <p> Julianna Dorsch!
                 </p>
-                <p>Whitney To</p>
+                <p>Whitney To!</p>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
