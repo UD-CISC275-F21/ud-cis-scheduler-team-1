667bc8dbc09ccd997703a82268f260929b256384
Megan Englert <megan.adare@live.com>
2021-10-14 13:34:15 -0400

    accidentally added only one file, simple table

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Semester } from "./components/semester";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler!
-                <p>Julianna Dorsch</p>
-                <p>Whitney To</p>
-                <p>Megan Englert</p>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
+            <Semester></Semester>
         </div>
     );
 }
--- src/components/semester.tsx
+++ src/components/semester.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 
-function Semester(): JSX.Element {
+export function Semester(): JSX.Element {
 
-    return <div className="semester">
-
-    </div>;
+    return <table className="semester">
+        <caption>Fall 2021</caption>
+        <tr>
+            <th>Course</th>
+            <th>Credits</th>
+        </tr>
+        <tr>
+            <td>EGGG101 Introduction to Engineering</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>CISC108 Introduction to Computer Science I</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>MATH241 Analytic Geometry and Calculus A</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>ENGL110 Seminar in Composition</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>ARTH237 Art in Tibet</td>
+            <td>3</td>
+        </tr>
+    </table>;
 }
