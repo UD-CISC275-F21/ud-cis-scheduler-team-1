55ef12d425fe43ae5a29210c0695965b00b419f9
judorsch <jdorsch@udel.edu>
2021-10-17 10:06:39 -0400

    getting table to render based on list

--- src/components/semester.tsx
+++ src/components/semester.tsx
@@ -1,41 +1,44 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Table } from "react-bootstrap";
 import "../App.css";
+
+/* Getting a table to render based on a list is from https://stackoverflow.com/questions/54659039/remove-table-row-using-hooks */
+
+const classes = [
+    {name: "EGGG101 Introduction to Engineering", credits: 2},
+    {name: "CISC108 Introduction to Computer Science I", credits: 3},
+    {name: "MATH241 Analytic Geometry and Calculus A", credits: 3},
+    {name: "ENGL110 Seminar in Composition", credits: 3},
+    {name: "ARTH237 Art in Tibet", credits: 3},
+
+];
+
+
 
 export function Semester(): JSX.Element {
 
     return <Col>
         <h2 className = "subtitle">Four Year Plan</h2>
-        <table className="semester">
+        <Table striped bordered hover className="semester">
             <thead>
-                <tr><th>{"Fall 2021"}</th></tr>
-            </thead>
-            <tbody>
+                <tr>
+                    <th>{"Fall 2021"}</th>
+                    <th>{""}</th>
+                </tr>
                 <tr>
                     <th>Course</th>
                     <th>Credits</th>
                 </tr>
-                <tr>
-                    <td>{"EGGG101 Introduction to Engineering"}</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>{"CISC108 Introduction to Computer Science I"}</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>{"MATH241 Analytic Geometry and Calculus A"}</td>
-                    <td>4</td>
-                </tr>
-                <tr>
-                    <td>{"ENGL110 Seminar in Composition"}</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>{"ARTH237 Art in Tibet"}</td>
-                    <td>3</td>
-                </tr>
+            </thead>
+            <tbody>
+                {classes.map((item, index) =>(
+                    <tr key ={index}>
+                        <td>{item.name}</td>
+                        <td>{item.credits}</td>
+                    </tr>
+
+                ))}
             </tbody>
-        </table>
+        </Table>
     </Col>;
 }
