381b99903d0d42d57335a22835b20e433958bdf9
Megan Englert <megan.adare@live.com>
2021-10-18 11:06:46 -0400

    each semester table is its own component - added singleSemesterViewer.tsx - currently errors

--- src/components/semesterViewer.tsx
+++ src/components/semesterViewer.tsx
@@ -23,107 +23,45 @@ export function SemesterViewer({ semesters, setSemesters }:
     // Adds a new semester with the given title (ex. "Fall 2021") and no courses
     function addSemester(title: string): void {
         setSemesters([...semesters,
-            {
-                title: title,
-                courses: []
-            }
+        {
+            title: title,
+            courses: []
+        }
         ]);
     }
 
-    const [numTable, setNumTable] = useState<Array<iNumTable>>(initialTable);
     return (
-        <Col md={8}>
+        <Row xs={1} md={2} className="g-4">
             <h2 className="subtitle">Four Year Plan</h2>
-            {numTable.map((item, index) =>
-                <Row key={index}>
-                    <Col>
-                        <Table striped bordered hover className="semester">
-                            <thead>
-                                <tr>
-                                    <th colSpan={3}>{"Fall 2021"}</th>
-                                </tr>
-                                <tr>
-                                    <th>Course</th>
-                                    <th>Credits</th>
-                                    <th>{""}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {classes.map((item, index) =>
-                                    <tr key={index}>
-                                        <td>{item.name}</td>
-                                        <td>{item.credits}</td>
-                                        <td>
-                                            <Button
-                                                color="red"
-                                                onClick={() => removeRow(item.name)}>
-                                                {" "}
-                                                X{" "}
-                                            </Button>
-                                        </td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </Table>
-                    </Col>
-                    <Col>
-                        <Table striped bordered hover className="semester">
-                            <thead>
-                                <tr>
-                                    <th colSpan={3}>{"Spring 2022"}</th>
-                                </tr>
-                                <tr>
-                                    <th>Course</th>
-                                    <th>Credits</th>
-                                    <th>{""}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {classes.map((item, index) =>
-                                    <tr key={index}>
-                                        <td>{item.name}</td>
-                                        <td>{item.credits}</td>
-                                        <td>
-                                            <Button
-                                                color="red"
-                                                onClick={() => {
-                                                    removeRow(item.name);
-                                                }}>
-                                                {" "}
-                                                X{" "}
-                                            </Button>
-                                        </td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-            )}
-            <ButtonGroup>
-                <Button
-                    onClick={() => {
-                        addT(numTable.length + 1);
-                    }}>
-                    + Add School Year
-                </Button>
-                <Button
-                    variant="danger"
-                    onClick={() => {
-                        if (numTable.length > 0) {
-                            removeTab(numTable.length);
-                        }
-                    }}>
-                    - Delete School Year
-                </Button>
-                <Button
-                    variant="secondary"
-                    onClick={() => {
-                        setNumTable(initialTable);
-                    }}>
-                    Reset Plan
-                </Button>
-            </ButtonGroup>
-        </Col>
+            {semesters.map((semester) =>
+                           
+        </Row>
+
+<Row>
+    <ButtonGroup>
+        <Button
+            onClick={() => {
+                addT(numTable.length + 1);
+            }}>
+            + Add School Year
+        </Button>
+        <Button
+            variant="danger"
+            onClick={() => {
+                if (numTable.length > 0) {
+                    removeTab(numTable.length);
+                }
+            }}>
+            - Delete School Year
+        </Button>
+        <Button
+            variant="secondary"
+            onClick={() => {
+                setNumTable(initialTable);
+            }}>
+            Reset Plan
+        </Button>
+    </ButtonGroup>
+</Row>
     );
 }
---
+++ src/components/singleSemesterViewer.tsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { Button, Col, Row, Table, ButtonGroup } from "react-bootstrap";
+import { Semester } from "../interfaces/semester";
+
+export function singleSemesterViewer({ semester }: { semester: Semester }): JSX.Element {
+
+    // Removes a course from a semester based on its name
+    function removeCourse(name: string): void {
+        semester.courses = [...semester.courses.filter(course => course.name != name)];
+    }
+
+    return <Col>
+        <Table striped bordered hover className="semester">
+            <thead>
+                <tr>
+                    <th colSpan={3}>{semester.title}</th>
+                </tr>
+                <tr>
+                    <th>Course</th>
+                    <th>Credits</th>
+                    <th>{""}</th>
+                </tr>
+            </thead>
+            <tbody>
+                {semester.courses.map((course) => (
+                    <tr>
+                        <td>{course.name}</td>
+                        <td>{course.credits}</td>
+                        <td>
+                            <Button
+                                color="red"
+                                onClick={() => removeCourse(course.name)}>
+                                {" "}X{" "}
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </Col>;
+}
