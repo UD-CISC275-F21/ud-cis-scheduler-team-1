be21f1ca7d4d543386782abe2658c0257fe15602
judorsch <jdorsch@udel.edu>
2021-10-26 12:15:12 -0400

    adding course based on input

--- src/utilities/findCourse.ts
+++ src/utilities/findCourse.ts
@@ -4,17 +4,8 @@ import Catalog from "../data/catalog.json";
 export function findCourse(name:string): Course{
     const code = name.substr(0, 4);
     console.log(code);
-    console.log(Catalog.ACCT["ACCT 166"]);
-    //const course:Course = Catalog.code[name];
-    //return course;
-    return{
-        code: "Acct 106",
-        name: "Accounting",
-        descr: "Take accounting",
-        credits: "3",
-        preReq: "",
-        restrict: "",
-        breadth: "",
-        typ: "fall",
-    };
+    console.log(Catalog["ACCT"]["ACCT 166"]);
+    const CATALOG_DATA: Record<string, Record<string, Course>> = Catalog;
+    const course:Course = CATALOG_DATA[code][name];
+    return course;
 }
