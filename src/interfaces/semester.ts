853cf5e70835e01e123cef46f2ba9edb94c355d0
Megan Englert <megan.adare@live.com>
2021-10-28 11:45:35 -0400

    maybe fixed some things?

--- src/interfaces/semester.ts
+++ src/interfaces/semester.ts
@@ -1,14 +1,12 @@
 import { Course } from "./course";
-
 export enum season {
     fall = "Fall",
     winter = "Winter",
     spring = "Spring",
     summer = "Summer"
 }
-
 export interface Semester {
     season: season
-    year: number
+    year: number    
     courses: Course[]
 }
