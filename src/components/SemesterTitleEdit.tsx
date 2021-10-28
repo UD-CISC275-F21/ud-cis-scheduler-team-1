920df3d7b24d052ee7b102a0a8780bf05e4350b6
Megan Englert <megan.adare@live.com>
2021-10-28 09:41:39 -0400

    semester titles are customizable

--- src/components/SemesterTitleEdit.tsx
+++ src/components/SemesterTitleEdit.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ButtonGroup, Dropdown, DropdownButton, Form } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import { Semester, season } from "../interfaces/semester";
 
 /*interface semesterTitleEdit {
@@ -9,14 +9,24 @@ import { Semester, season } from "../interfaces/semester";
 }*/
 
 export function SemesterTitleEdit(): JSX.Element {
-    return <div>
-        <Form>
-            <Form.Select aria-label="Select season">
-                <option>Fall</option>
-                <option>Winter</option>
-                <option>Spring</option>
-                <option>Summer</option>
-            </Form.Select>
-        </Form>
-    </div>;
+    const years = Array.from(Array(30).keys()).map(x => x + 1995);
+
+    return <Form>
+        <Row className="g-2">
+            <Col>
+                <Form.Select aria-label="Select season">
+                    <option key={season.fall}>Fall</option>
+                    <option key={season.winter}>Winter</option>
+                    <option key={season.spring}>Spring</option>
+                    <option key={season.summer}>Summer</option>
+                </Form.Select>
+            </Col>
+            <Col>
+                <Form.Select aria-Label="Select year" defaultValue={2021}>
+                    <option>Select Year</option>
+                    {years.map((year) => <option key={year}>{year}</option>)}
+                </Form.Select>
+            </Col>
+        </Row>
+    </Form>;
 }
