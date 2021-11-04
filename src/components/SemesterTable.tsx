5cff421aac3e65cc0c3b0d600bb6a27c309af517
Whitneyto <nguyetto@udel.edu>
2021-11-03 21:53:00 -0400

    Deep copy mod

--- src/components/SemesterTable.tsx
+++ src/components/SemesterTable.tsx
@@ -88,7 +88,7 @@ export function SemesterTable({sem, setSemesters, semesters}: semesterTable): JS
                                 <a
                                     onClick={() => {
                                         setShow(true);
-                                        setMod({...course});
+                                        setMod(JSON.parse(JSON.stringify(course)));
                                     }}>
                                     {course.info.code}
                                     <br></br>
