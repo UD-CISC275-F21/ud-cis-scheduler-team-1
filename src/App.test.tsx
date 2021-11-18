8b3b4d44e4c3b94b23e6dd9c5d3570a0eb30387a
Megan Englert <75327712+meganenglert@users.noreply.github.com>
2021-11-18 11:34:00 -0500

    pushing tests while panicking about merge conflicts in package lock that should not exist

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import { updateCSBS } from "./data/csBS";
+
+test("renders UD CIS Scheduler text", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/UD CIS Scheduler/i);
+    expect(linkElement).toBeInTheDocument();
+});
 
 describe("App", () => {
+
     beforeEach(() => {
         render(<App />);
-    });
-    
-    it("renders UD CIS Scheduler text", () => {
-        const linkElement = screen.getByText(/UD CIS Scheduler/i);
-        expect(linkElement).toBeInTheDocument();
     });
 
     describe("CoursePool", () => {
@@ -17,7 +20,35 @@ describe("App", () => {
     });
 
     describe("Concentrations", () => {
-        console.log("yay");
+
+        describe("Elements render correctly on app load", () => {
+
+            it("has the degree type dropdown when the app loads", () => {
+                const element = screen.queryByTestId("degree-select");
+                expect(element).toBeInTheDocument();
+            });
+            it("has the concentration select visible when the app renders", () => {
+                expect(screen.getByTestId("concentration-row")).toBeInTheDocument();
+            });
+            it("selects BS program with traditional program concentration upon load", () => {
+                expect(screen.queryByText("Major")).toBeInTheDocument();
+                expect(screen.queryByText("BS")).toBeInTheDocument();
+                expect(screen.queryByText("Traditional Program")).toBeInTheDocument();
+            });
+
+        });
+
+        describe("Major/Minor dropdown", () => {
+
+            // https://jacobwicks.github.io/2020/05/16/testing-semantic-ui-react-dropdown.html
+            it("enables and disables BA/BS and concentration correctly", () => {
+                const dropdown = screen.getByTestId("major-minor");
+                fireEvent.click(dropdown);
+                const options = screen.getAllByTestId("option-majorminor");
+                fireEvent.click(options[1]);
+                expect(options[1].textContent).toBeInTheDocument();
+            });
+        });
     });
 
     describe("FourYearPlan", () => {
