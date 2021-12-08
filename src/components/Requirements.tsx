e96cea59fd8edcf22f82cc947669c37753a73e04
judorsch <jdorsch@udel.edu>
2021-12-08 02:37:36 -0500

    adding url to see more info

--- src/components/Requirements.tsx
+++ src/components/Requirements.tsx
@@ -14,9 +14,36 @@ interface iReq {
     conc: string;
 }
 
+export function getURLConc(conc:string):string{
+    const concURLS : Record<string, string> = {
+        "Traditional Program": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34727&returnto=8860",
+        "Artificial Intelligence and Robotics": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34982&returnto=8860",
+        "Bioinformatics": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34983&returnto=8860",
+        "Cybersecurity": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34981&returnto=8860",
+        "Data Science": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34980&returnto=8860",
+        "High Performance Computing": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34984&returnto=8860",
+        "Systems and Networks": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34985&returnto=8860",
+        "Theory and Computation": "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34979&returnto=8860",
+    };
+    return concURLS[conc];
+}
+
+export function getUrl(bsba:string, major:string, conc:string): string{
+    if(major === "Minor"){
+        return "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34733&returnto=8860";
+    } else{
+        if(bsba === "BA"){
+            return "https://catalog.udel.edu/preview_program.php?catoid=47&poid=34726&returnto=8860";
+        }else{
+            return getURLConc(conc);
+        }
+    }
+}
+
 export function Requirements({ semesters, bsba, major, conc }: iReq): JSX.Element {
     const [remainingCourses,setRemainingCourses] = useState<string[]>([]);
     const [fulfilledCourses, setFulfilledCourses] = useState<string[]>([]);
+    const url = getUrl(bsba, major, conc);
 
     function getConc() {
         if (major === "Minor") {
@@ -28,7 +55,6 @@ export function Requirements({ semesters, bsba, major, conc }: iReq): JSX.Elemen
                 return findConcentration(conc,semesters);
             }
         }
-        return CSMinorUpdate(semesters);
     }
     useEffect(() => {
         const newRemCourses : string[] = [];
@@ -72,6 +98,7 @@ export function Requirements({ semesters, bsba, major, conc }: iReq): JSX.Elemen
     }, [semesters,bsba,major,conc] );
     return <Col id="move-when-scroll">
         <h2 className="subtitle">Degree Requirements</h2>
+        <p>To see more details about degree/minor requirements, click <a href = {url} target="_blank" rel="noopener noreferrer">here</a>.</p>
         <ul>
             <ReqCourseText courseKeys={remainingCourses} fulfilled={false}></ReqCourseText>
             <ReqCourseText courseKeys={fulfilledCourses} fulfilled={true}></ReqCourseText>
